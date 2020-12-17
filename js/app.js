94ab66c2e3b552ab3a126f4bda56490d254bcf2e
nicolas <nicolas-simonet@live.fr>
2020-12-17 10:57:50 +0100

    commit1

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const btnMenu = document.querySelector('.logo-menu');
+const menu = document.querySelector('.liste-nav');
+
+btnMenu.addEventListener('click', function(){
+    menu.classList.toggle('active');
+
+})
